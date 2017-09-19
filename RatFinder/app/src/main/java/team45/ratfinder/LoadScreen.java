35d9e447c2058cbe62858390661d3e1a000b444d
Janet Tanzy <janettanzy@lawn-128-61-67-121.lawn.gatech.edu>
2017-09-19 15:41:01 -0400

    have added xml files

---
+++ RatFinder/app/src/main/java/team45/ratfinder/LoadScreen.java
@@ -0,0 +1,13 @@
+package team45.ratfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+    }
+}
