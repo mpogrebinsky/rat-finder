a1348ba9a5b5926f186198826e3cdc8f03080be1
jtanzy6 <31421909+jtanzy6@users.noreply.github.com>
2017-09-27 22:13:47 -0400

    register xmls and page transition

--- RatFinder/app/src/main/java/team45/ratfinder/MainActivity.java
+++ RatFinder/app/src/main/java/team45/ratfinder/MainActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static EditText username;
+    private static EditText password;
+    private static TextView errorMessage;
+    private static Button loginButton;
+    private static Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button submit = (Button) findViewById(R.id.submit_id);
-        submit.setOnClickListener(new View.OnClickListener() {
+        errorMessage = (TextView) findViewById(R.id.errorMessage);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        errorMessage.setVisibility(View.INVISIBLE);
+        loginButton = (Button) findViewById(R.id.submit_id);
+        registerButton = (Button) findViewById(R.id.cancel);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, StartActivity.class);
-                startActivity(intent);
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
+                    Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                    startActivity(intent);
+                } else {
+                    errorMessage.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                {
+                    Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
---
+++ RatFinder/app/src/main/java/team45/ratfinder/RegisterActivity.java
@@ -0,0 +1,19 @@
+package team45.ratfinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by janettanzy on 9/27/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
